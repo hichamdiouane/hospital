73508deed22e8474c29d859dfe2d8bf8669e8f9c
HICHAM DIOUANE <hichamdiouane9@gmail.com>
2025-03-17 10:34:10 +0000

    premier test de l'application en utilisant h2 console

--- src/main/java/ma/enset/hospital/HospitalApplication.java
+++ src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -1,13 +1,29 @@
 package ma.enset.hospital;
 
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
+
 @SpringBootApplication
-public class HospitalApplication {
+public class HospitalApplication implements CommandLineRunner {
+
+	@Autowired
+	private PatientRepository patientRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		patientRepository.save(new Patient(null, "Hassan", new Date(), false, 12));
+		patientRepository.save(new Patient(null, "Khalid", new Date(), true, 34));
+		patientRepository.save(new Patient(null, "Omar", new Date(), false, 56));
+	}
 }
---
+++ src/main/java/ma/enset/hospital/entities/Patient.java
@@ -0,0 +1,20 @@
+package ma.enset.hospital.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private Boolean malade;
+    private int score;
+}
